1bba06f82a292040d5a85a11bc40f0084eefbcf6
Teodora Krstic <teodora.krstic0109@gmail.com>
2023-08-28 08:57:23 +0200

    Initial commit with work on login page

---
+++ app/src/main/java/com/example/padnica_zoo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.padnica_zoo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, passwordEditText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                // Perform login validation here
+                if (username.equals("your_username") && password.equals("your_password")) {
+                    // Login successful, you can navigate to the next activity
+                } else {
+                    // Login failed, show an error message
+                }
+            }
+        });
+    }
+}
