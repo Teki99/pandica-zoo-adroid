48aedc246fd4c0b3dfcecc8cefaa77a981a26684
Филип Јанковић <jf160343d@student.etf.bg.ac.rs>
2023-09-23 09:45:38 +0200

    Added home page

--- app/src/main/java/com/example/padnica_zoo/ui/animals/AnimalsFragment.java
+++ app/src/main/java/com/example/padnica_zoo/ui/animals/AnimalsFragment.java
@@ -1,4 +1,6 @@
 package com.example.padnica_zoo.ui.animals;
 
-public class AnimalsFragment {
+import androidx.fragment.app.Fragment;
+
+public class AnimalsFragment extends Fragment {
 }
--- app/src/main/java/com/example/padnica_zoo/ui/events/EventsFragment.java
+++ app/src/main/java/com/example/padnica_zoo/ui/events/EventsFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
